d005735389799c56c7d0d742d5b61cfbf7b0cd98
Kazuma NAGAI <itb4769we@gmail.com>
2021-07-02 21:42:40 +0900

    InvoiceまたはPaymentをHTMLにレンダリング

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import { Invoice } from "./models/Invoice.js";
-import { Payment } from "./models/Payment.js";
+import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
@@ -10,6 +11,10 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul)
+
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
@@ -19,5 +24,6 @@ form.addEventListener('submit', (e: Event) => {
   } else {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
-  console.log(doc);
+
+  list.render(doc, type.value, 'end');
 })
