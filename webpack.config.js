0ec642ef495ffceb96590ac2061b83d36017b9aa
Kazuma NAGAI <itb4769we@gmail.com>
2021-07-08 18:57:00 +0900

    TypeScriptのソースマップの設定

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const MODE = "development";
 const enabledSourceMap = MODE === "development";
 
 module.exports = {
+  devtool: 'eval-source-map',
   mode: MODE,
   resolve: {
     extensions: ['.js', '.ts'],
-    // modules: [path.resolve(__dirname, 'src'), 'node_modules']
   },
   entry: './src/app.ts',
   module: {
@@ -19,27 +19,18 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          // linkタグに出力する機能
           "style-loader",
-          // CSSをバンドルするための機能
           {
             loader: "css-loader",
             options: {
-              // オプションでCSS内のurl()メソッドの取り込みを禁止する
               url: false,
-              // ソースマップの利用有無
               sourceMap: enabledSourceMap,
-
-              // 0 => no loaders (default);
-              // 1 => postcss-loader;
-              // 2 => postcss-loader, sass-loader
               importLoaders: 2
             }
           },
           {
             loader: "sass-loader",
             options: {
-              // ソースマップの利用有無
               sourceMap: enabledSourceMap
             },
           }
